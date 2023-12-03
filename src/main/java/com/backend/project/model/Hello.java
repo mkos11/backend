38c65901bc20eb7b6ccb6a4964182505602425f1
Pushpak Jain <pushpakjain628@gmail.com>
2023-12-03 20:26:22 +0530

    Initial Project setup

---
+++ src/main/java/com/backend/project/model/Hello.java
@@ -0,0 +1,12 @@
+package com.backend.project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hello {
+    private String name;
+}
